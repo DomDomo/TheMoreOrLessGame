b9777fa96cb4a43a58a0696fcd11da2ce3a94486
DomDomo <g.domis02@gmail.com>
2021-04-07 04:37:55 +0300

    Add responsive video titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './index.css'
 
-const Hello = ({style}) => {
+const Side = (props) => {
   return (
-    <div className={[style, "half"].join(' ')}>
-      <p>Hello world</p>
+    <div className={[props.style, "half"].join(' ')}>
+      <h2>"{props.video.name}"</h2>
+      <p>has</p>
+      <h1>{props.video.views}</h1>
+      <p>views</p>
+    </div>
+  )
+}
+
+const Disc = ({style}) => {
+  return (
+    <div className="disc">
+      <h1>VS</h1>
     </div>
   )
 }
@@ -16,10 +27,22 @@ const App = () => {
     error: "error"
   }
 
+  const video1 = {
+    name: "Hello world",
+    views: "500k"
+  };
+  const video2 = {
+    name: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.",
+    views: "27M"
+  };
+
   return (
-    <div className="splitScreen">
-      <Hello style={styles.success}/>
-      <Hello style={styles.error}/>
+    <div className="mainView">
+      <div className="splitScreen">
+        <Side style={styles.success} video={video1}/>
+        <Side style={styles.error} video={video2}/>
+    </div>
+    <Disc />
   </div>
   )
 }
