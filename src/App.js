ec8282113aba44bba0912f012ba7b3f28535eff3
DomDomo <g.domis02@gmail.com>
2021-04-08 05:53:51 +0300

    Add fade-in and move up animations for view container

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const Roomba = require( "./images/Roomba.jpg" );
 
 const Buttons = ({handleClick}) => {
   return (
-    <div >
+    <div className="buttonContainer">
       <button onClick={handleClick} className="game-button more">More <span className="arrow-up"></span></button>
       <button onClick={handleClick} className="game-button less">Less <span className="arrow-down"></span></button>
     </div>
@@ -15,19 +15,21 @@ const Buttons = ({handleClick}) => {
 }
 
 const ViewView = (props) => {
+
+
   const viewsToShow = props.video.first ? props.video.views : props.views;
   // This regex adds commas after 3 digits to make a formatted (locale) string
   const formattedViews = viewsToShow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
-      <p className="videoViews">{formattedViews}</p>
+      <p className="videoViews" >{formattedViews}</p>
   )
 }
 
 const Side = (props) => {
 
   const { countUp, start } = useCountUp({
-    start: 0,
+    start: props.video.views - props.video.views*0.2,
     end: props.video.views,
     // Coulnd't find a way to change by bool so put a very long dealy
     delay: 10000000,
@@ -44,16 +46,22 @@ const Side = (props) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.video.showViews]);
 
+  const viewContainerClass = props.video.showViews ? "viewContainer fadeIn" : "viewContainer";
+
+
   return (
     <div className="half" style={{backgroundImage: `url(${props.video.image})`}}>
+      <div className="top">
       <p className="videoTitle">"{props.video.name}"</p>
       <p>has</p>
-      
-      {props.video.showViews
-        ? <ViewView views={countUp} video={props.video}/>
-        : <Buttons handleClick={props.handleClick}/>
-      }
+      </div>
+      <div className="bottom">
+      {!props.video.showViews && <Buttons handleClick={props.handleClick}/>}
+      <div className={viewContainerClass}>
+      {props.video.showViews && <ViewView views={countUp} video={props.video}/>}
+      </div>
       <p >views</p>
+      </div>
     </div>
   )
 }
