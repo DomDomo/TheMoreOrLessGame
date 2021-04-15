dd4fe9ec1729a4591198d5a27cb9a1f709da0efa
DomDomo <g.domis02@gmail.com>
2021-04-15 22:36:19 +0300

    Add new video

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useCountUp } from 'react-countup';
+import { useCountUp } from "react-countup";
 
-import './index.css'
-const Cicada = require( "./images/3301.jpg" );
-const Roomba = require( "./images/Roomba.jpg" );
-const Fallout = require( "./images/76.jpg" );
+import "./index.css";
+const Cicada = require("./images/3301.jpg");
+const Roomba = require("./images/Roomba.jpg");
+const Fallout = require("./images/76.jpg");
+const IsShe = require("./images/Is_She.jpg");
 
-const Buttons = ({handleClick}) => {
+const Buttons = ({ handleClick }) => {
   return (
     <div className="buttonContainer">
-      <button onClick={handleClick} className="game-button more">More <span className="arrow-up"></span></button>
-      <button onClick={handleClick} className="game-button less">Less <span className="arrow-down"></span></button>
+      <button onClick={handleClick} className="game-button more">
+        More <span className="arrow-up"></span>
+      </button>
+      <button onClick={handleClick} className="game-button less">
+        Less <span className="arrow-down"></span>
+      </button>
       <p className="pNextToButtons">views</p>
     </div>
-  )
-}
+  );
+};
 
 const ViewView = (props) => {
-
-
   const viewsToShow = props.video.leftSide ? props.video.views : props.views;
   // This regex adds commas after 3 digits to make a formatted (locale) string
-  const formattedViews = viewsToShow.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const formattedViews = viewsToShow
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-  return (
-      <p className="videoViews" >{formattedViews}</p>
-  )
-}
+  return <p className="videoViews">{formattedViews}</p>;
+};
 
 const Side = (props) => {
-
   const { countUp, start } = useCountUp({
-    start: props.video.views - props.video.views*0.2,
+    start: props.video.views - props.video.views * 0.2,
     end: props.video.views,
     // Coulnd't find a way to change by bool so put a very long dealy
     delay: 10000000,
-    duration: 1
+    duration: 1,
   });
 
   const isInitialMount = useRef(true);
@@ -45,41 +47,44 @@ const Side = (props) => {
     } else {
       start();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.video.showViews]);
 
-  const viewContainerClass = props.video.showViews ? "viewContainer fadeIn" : "viewContainer";
-
+  const viewContainerClass = props.video.showViews
+    ? "viewContainer fadeIn"
+    : "viewContainer";
 
   return (
-    <div className="half" style={{backgroundImage: `url(${props.video.image})`}}>
+    <div
+      className="half"
+      style={{ backgroundImage: `url(${props.video.image})` }}
+    >
       <div className="top">
-      <p className="videoTitle">"{props.video.name}"</p>
-      <p>has</p>
+        <p className="videoTitle">"{props.video.name}"</p>
+        <p>has</p>
       </div>
       <div className="bottom">
-        {!props.video.showViews && <Buttons handleClick={props.handleClick}/>}
+        {!props.video.showViews && <Buttons handleClick={props.handleClick} />}
         <div className={viewContainerClass}>
-        {props.video.showViews && <ViewView views={countUp} video={props.video}/>}
-        <p>views</p>
+          {props.video.showViews && (
+            <ViewView views={countUp} video={props.video} />
+          )}
+          <p>views</p>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Disc = () => {
   return (
     <div className="disc">
       <h1>VS</h1>
     </div>
-  )
-}
-
+  );
+};
 
 const App = () => {
-
   const [show, setShow] = useState(false);
   const [win, setWin] = useState(false);
 
@@ -105,6 +110,13 @@ const App = () => {
     showViews: show,
     leftSide: false,
   };
+  const video4 = {
+    name: "Casually Explained: Is She Into You?",
+    views: 16012073,
+    image: IsShe.default,
+    showViews: show,
+    leftSide: false,
+  };
 
   const handleClick = () => {
     setShow(true);
@@ -116,16 +128,15 @@ const App = () => {
   return (
     <div className="mainView">
       <div className={gameScroller}>
-        <Side video={video1}/>
-        <Side handleClick={handleClick} video={video2}/>
-        <Side handleClick={handleClick} video={video3}/>
+        <Side video={video1} />
+        <Side handleClick={handleClick} video={video2} />
+        <Side handleClick={handleClick} video={video3} />
+      </div>
+      <Disc />
     </div>
-    <Disc />
-  </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
 
 //<Side handleClick={handleClick} video={video3}/>
