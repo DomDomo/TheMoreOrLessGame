e23e100fc88d126399178fcac55881d276626ab7
DomDomo <g.domis02@gmail.com>
2021-04-23 23:34:54 +0300

    Extract videos in their own file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useCountUp } from "react-countup";
+import { mainVids } from "./videos";
 
 import "./index.css";
-const Cicada = require("./images/3301.jpg");
-const Roomba = require("./images/Roomba.jpg");
-const Fallout = require("./images/76.jpg");
-const IsShe = require("./images/Is_She.jpg");
-
-const mainVids = [
-  {
-    name: "Cicada 3301: An Internet Mystery",
-    views: 23984345,
-    image: Cicada.default,
-    showViews: true,
-    leftSide: true,
-  },
-  {
-    name: "The Roomba That Screams When it Bumps Into Stuff",
-    views: 21460437,
-    image: Roomba.default,
-    showViews: false,
-    leftSide: false,
-  },
-  {
-    name: "The Fall of 76",
-    views: 25250766,
-    image: Fallout.default,
-    showViews: false,
-    leftSide: false,
-  },
-  {
-    name: "Casually Explained: Is She Into You?",
-    views: 16012073,
-    image: IsShe.default,
-    showViews: false,
-    leftSide: false,
-  },
-];
 
 const Buttons = ({ handleClick }) => {
   return (
