cd264bade78b08823f746078bc15f988aedfb9c4
DomDomo <g.domis02@gmail.com>
2021-08-10 19:20:50 +0300

    Improve homepage

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,10 @@ function Home() {
   return (
     <div className="homePage">
       <header>
-        <h1>The More or Less Game</h1>
+        <h1 className="startHeader">
+          The <span className="moreWord">More</span> or{" "}
+          <span className="lessWord">Less</span> Game
+        </h1>
 
         <Link to="/game" style={{ textDecoration: "none" }}>
           <button className="game-button start-game">Start</button>
