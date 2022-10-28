55ebf1c195e5360c8489aa80b72145cbebdf0ec6
DomDomo <g.domis02@gmail.com>
2022-10-28 22:08:20 +0100

    Update .env variables

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -3,13 +3,12 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "themoreorlessgame.firebaseapp.com",
-  databaseURL:
-    "https://themoreorlessgame-default-rtdb.europe-west1.firebasedatabase.app",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: "themoreorlessgame.appspot.com",
-  messagingSenderId: "26407639457",
-  appId: "1:26407639457:web:d27ed079c98c19c73393e0",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
